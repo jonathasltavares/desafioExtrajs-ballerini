3ba33d31e6f1c0d3fe3bda88dccfb3de0a36e403
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-02-08 13:34:33 -0300

    teste

---
+++ script.js
@@ -0,0 +1,45 @@
+botao = document.getElementById("botao_cadastrar")
+botao.addEventListener("click", cadastrar)
+
+resnome1 = document.getElementById("resnome1")
+resnome2 = document.getElementById("resnome2")
+resnome3 = document.getElementById("resnome3")
+
+residade1 = document.getElementById("residade1")
+residade2 = document.getElementById("residade2")
+residade3 = document.getElementById("residade3")
+
+resskills1 = document.getElementById("resskills1")
+resskills2 = document.getElementById("resskills2")
+resskills3 = document.getElementById("resskills3")
+
+resUsernome1 = document.getElementById("resUsuarionome1")
+resUsernome2 = document.getElementById("resUsuarionome2")
+resUsernome3 = document.getElementById("resUsuarionome3")
+
+resUseridade1 = document.getElementById("resUsuarioidade1")
+resUseridade2 = document.getElementById("resUsuarioidade2")
+resUseridade3 = document.getElementById("resUsuarioidade3")
+
+resUserskills1 = document.getElementById("resUsuarioskills1")
+resUserskills2 = document.getElementById("resUsuarioskills2")
+resUserskills3 = document.getElementById("resUsuarioskills3")
+
+
+
+function cadastrar(){
+    var i = 0
+    i++
+    if(i==1){
+        resnome1 = innerHTML = "Nome1"+i
+        
+    }else if(i==2){
+        resnome2 = innerHTML = "Nome2"+i
+        
+    }else if(i==3){
+        resnome3 = innerHTML = "Nome3"+i
+
+    }
+    
+
+}
